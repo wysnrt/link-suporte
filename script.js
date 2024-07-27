2f6a462cf71f5ac6313dc00077db38b66cd6e74a
wysnrt <wendelltahara@gmail.com>
2024-07-27 12:24:03 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    var loginForm = document.getElementById('loginForm');
+    
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        var password = document.getElementById('password').value;
+        
+        // Verifica a senha. Substitua 'senha123' com a senha desejada
+        if (password === '43690') {
+            var expirationTime = new Date().getTime() + (10 * 10 * 600); // 1 hora de expiração
+            localStorage.setItem('authenticated', 'true');
+            localStorage.setItem('expiration', expirationTime);
+            window.location.href = 'index.html';
+        } else {
+            alert('Senha incorreta. Tente novamente.');
+        }
+    });
 
-    // Obter valores do formulário
-    //var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    // Validação simples (usuário e senha hardcoded)
-    //if (username === 'suporte' && password === '43690') 
-    if (password === '43690'){
-        //alert('Login bem-sucedido!');
-        localStorage.setItem('authenticated', 'true');
-        // Aqui você pode redirecionar o usuário para outra página ou fazer outra ação desejada
+    // Redireciona para index.html se já estiver autenticado e a sessão não estiver expirada
+    var authenticated = localStorage.getItem('authenticated');
+    var expiration = localStorage.getItem('expiration');
+    if (authenticated === 'true' && new Date().getTime() < expiration) {
         window.location.href = 'index.html';
-    } else {
-        alert('Credenciais inválidas. Tente novamente.');
     }
 });
+
