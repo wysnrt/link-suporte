738c395c042b8b10713d8f0cbb226543e7f60b80
wysnrt <wendelltahara@gmail.com>
2024-10-23 11:38:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // Verifica a senha. Substitua 'senha123' com a senha desejada
         if (password === '43690') {
-            var expirationTime = new Date().getTime() + (10 * 1000); // 10 segundos de expiração
+            var expirationTime = new Date().getTime() + (1 * 1000); // 10 segundos de expiração
             console.log('Senha correta. Definindo expiração para:', expirationTime);
             localStorage.setItem('authenticated', 'true');
             localStorage.setItem('expiration', expirationTime);
@@ -21,12 +21,14 @@ document.addEventListener('DOMContentLoaded', function() {
     // Redireciona para index.html se já estiver autenticado e a sessão não estiver expirada
     var authenticated = localStorage.getItem('authenticated');
     var expiration = localStorage.getItem('expiration');
+    var currentTime = new Date().getTime();
+    
     console.log('Authenticated:', authenticated);
     console.log('Expiration:', expiration);
-    console.log('Current Time:', new Date().getTime());
+    console.log('Current Time:', currentTime);
 
 
-    if (authenticated === 'true' && new Date().getTime() < expiration) {
+    if (authenticated === 'true' && currentTime < expiration) {
         window.location.href = 'index.html';
     }
 });
